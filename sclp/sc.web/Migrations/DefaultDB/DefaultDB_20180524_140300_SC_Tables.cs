8fbde12c8d1c7395a83e2cc01d123ad4769b20ac
Alexandre Pezzini <alexandre_pezzini@hotmail.com>
2018-05-24 18:11:50 -0300

    bug fixes

--- sclp/sc.web/Migrations/DefaultDB/DefaultDB_20180524_140300_SC_Tables.cs
+++ sclp/sc.web/Migrations/DefaultDB/DefaultDB_20180524_140300_SC_Tables.cs
@@ -5,6 +5,6 @@ namespace sc.Migrations.DefaultDB {
     [Migration(20180524131100)]
     public class DefaultDB_20180524_131100_SC_Initial : ForwardOnlyMigration {
 
-        public override void Up() => Execute.EmbeddedScript("0_CreateSchema.sql");
+        public override void Up() => Execute.EmbeddedScript("SC_Initial.sql");
     }
 }
